8c6c32687c40f7f4d565ca457f110e3639a63d71
AitorCour <aitorcour@gmail.com>
2020-04-10 20:20:04 +0200

    first

---
+++ VisualNovel/Assets/Scripts/Character.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    string characterName = "";
+    Sprite characterSprite = null; //characterPoses 0
+    Sprite[] characterPoses = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
